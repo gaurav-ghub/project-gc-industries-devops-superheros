f306e185c1598b975114823482e67f2984c3380c
gc-ghub <161034127+gc-ghub@users.noreply.github.com>
2026-05-18 13:16:59 +0530

    feat: frontend update adding icons for githib and linkedin

--- gc-industries-devops-superheros/superhero-app-code/frontend/src/components/Layout.jsx
+++ gc-industries-devops-superheros/superhero-app-code/frontend/src/components/Layout.jsx
@@ -11,6 +11,30 @@ export default function Layout({ children }) {
       <header className="topbar">
         <div className="topbar-inner">
           <div className="brand">GC Industries — <span className="ver">Shipping SuperHeros into the World of DevOps</span></div>
+          {/* NEW SOCIAL ICONS */}
+          <div className="social-icons">
+      <a
+        href="https://github.com/gc-ghub"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/github.svg"
+          alt="GitHub"
+        />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/gc-devops-world"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/linkedin.svg"
+          alt="LinkedIn"
+        />
+      </a>
+    </div>
           <nav className="nav">
             <Link to="/" className={`nav-btn ${loc.pathname === '/' ? 'active' : ''}`}>🏡 Home</Link>
             <Link to="/catalog" className={`nav-btn ${loc.pathname.startsWith('/catalog') ? 'active' : ''}`}>📦 Catalog</Link>
